6184dca50710269c84a9c1eea3fb8f90378c9b10
Navendra <navendra.singh@unthinkable.co>
2022-10-12 15:04:22 +0530

    Custom Card

--- App.js
+++ App.js
@@ -8,9 +8,8 @@ export default function App() {
     <SafeAreaView>
       <ScrollView>
         <View style={styles.container}>
-          <CardImage />
-          <CardImage />
-          <CardImage />
+
+        
           <CardImage />
         </View>
       </ScrollView>
